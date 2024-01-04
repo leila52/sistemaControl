70b3e2e9cb498c00e3f8c7cf9ffa84862a707b16
leila_16 <leila_16@leila>
2024-01-04 20:18:07 +0100

    primer comit del master en triangulo

--- sistemadecontrol/src/sistemadecontrol/ej_compartido.java
+++ sistemadecontrol/src/sistemadecontrol/ej_compartido.java
@@ -1,10 +1,22 @@
 package sistemadecontrol;
 
+import java.util.Scanner;
+
 public class ej_compartido {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner (System.in);
+		//el area del triangulo
+		System.out.println("Introduce la altura del triangulo: ");
+		int altura = entrada.nextInt();
+		System.out.println("Introduce la base del triangulo : ");
+		int base = entrada.nextInt();
+		System.out.println("el area del triangulo es : "+calcularArea(base,altura));
+		
+		
+		
+    }public static double calcularArea(int base, int altura) {
+	return base * altura / 2;
 
-	}
-
+    }
 }
