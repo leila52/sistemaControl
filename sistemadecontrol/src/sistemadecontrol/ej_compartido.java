640f5f771c6f8d6ec153683f4401c71ca24b94b3
ainhoa32 <ainhoa32alarcon@gmail.com>
2024-01-04 20:27:37 +0100

    segundo commit, área y perímetro círculo

--- sistemadecontrol/src/sistemadecontrol/ej_compartido.java
+++ sistemadecontrol/src/sistemadecontrol/ej_compartido.java
@@ -13,10 +13,21 @@ public class ej_compartido {
 		int base = entrada.nextInt();
 		System.out.println("el area del triangulo es : "+calcularArea(base,altura));
 		
+		//área y perímetro del círculo
+		int radio = entrada.nextInt();
+		System.out.println("el area de la circunferencia es: "+calaACirc(radio));
+		System.out.println("el perimetro de la circunferencia es : "+perimetroACirc(radio));
 		
 		
     }public static double calcularArea(int base, int altura) {
 	return base * altura / 2;
 
-    }
+	//Alumno 2 calcula el área y el perímetro del círculo
+    }public static double calaACirc(int radio) {
+		double area = 3.14*(radio^2);
+		return area;
+	}public static double perimetroACirc(int radio) {
+		double perimetro=2*(3.14*radio);
+		return perimetro;
+	}
 }
